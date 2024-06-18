c349e8857f86d3d9022034ffe3b399e11bdd895c
Ahmed Khan <ahmed.khan@smallcase.com>
2024-06-18 14:16:39 +0000

    test cases added

---
+++ src/tests/jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testPathIgnorePatterns: ['/node_modules/'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
+
+console.log('Jest is running the following files:');
